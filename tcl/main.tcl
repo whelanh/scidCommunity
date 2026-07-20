a627461cc99121d98450f262c7e299bb163b3583
whelanh <brickhousedevelopers@gmail.com>
2026-07-20 16:58:56 -0400

    Main Board actions

--- tcl/main.tcl
+++ tcl/main.tcl
@@ -1582,6 +1582,22 @@ proc CreateMainBoard { {w} } {
   updateTitle
 }
 
+# ::toggleMainBoard
+#   Restores the main board window if it was closed, or raises/focuses it.
+proc ::toggleMainBoard {} {
+  if {[winfo exists .main]} {
+    lassign [::win::isDocked .main] docked_nb w
+    if {$docked_nb eq ""} {
+      wm deiconify $w
+      raise $w
+      focus $w
+    }
+    return
+  }
+  ::CreateMainBoard .main
+  updateBoard -pgn
+}
+
 proc CreateGameInfo {} {
   # .gameInfo is the game information widget:
   #
